c32ffe8c77c7bd1b95947bc70d34ca4309891436
ayoubjebb2001 <jebbouri1968@gmail.com>
2024-12-24 10:21:32 +0100

    Add initial database schema for library management system

---
+++ db.sql
@@ -0,0 +1,45 @@
+-- Base de données : Bibliothèque
+CREATE DATABASE Bibliotheque;
+USE Bibliotheque;
+
+-- Table des utilisateurs
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'authenticated', 'visitor') DEFAULT 'visitor',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Table des catégories de livres
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+);
+
+-- Table des livres
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    category_id INT NOT NULL,
+    cover_image VARCHAR(255), 
+    summary TEXT,
+    status ENUM('available', 'borrowed', 'reserved') DEFAULT 'available',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
+
+-- Table des emprunts
+CREATE TABLE borrowings (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    book_id INT NOT NULL,
+    borrow_date DATE NOT NULL,
+    due_date DATE NOT NULL,
+    return_date DATE DEFAULT NULL,
+    notification_sent TINYINT(1) DEFAULT 0, -- 1 si un e-mail a été envoyé
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE
+);
